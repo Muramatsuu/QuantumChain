6ab428f17adea552b32e4f8fb03efeb57fce586f
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 16:22:35 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/quantumchain/main.go
@@ -0,0 +1,23 @@
+// cmd/quantumchain/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"quantumchain/internal/quantumchain"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := quantumchain.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
